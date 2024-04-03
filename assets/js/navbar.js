453b7c0349a29f89e7c3162e7bea4b6dce457029
unknown <monalisuriarachchi@gmail.com>
2024-04-03 22:55:51 +0530

    added user profile and team

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(){
-    document.querySelector('body').insertAdjacentHTML("beforebegin", `
+    document.querySelector('body').insertAdjacentHTML("afterbegin", `
         <header>
             <div class="navigation_bar">
                 <div class="navigation_bar_logo">
@@ -10,9 +10,20 @@ window.addEventListener("load", function(){
                 <a href="shop.html">Shop</a>
                 <a href="gallery.html">Gallery</a>
                 <a href="feedback.html">Feedback</a>
-                <a href="aboutus.html">About us</a>
-                <a href="register.html">Register</a>
+                <a href="team.html">About us</a>
+                <a href="profile.html">Register</a>
             </div>
         </header>
+    `)
+    document.querySelector('body').insertAdjacentHTML("beforeend", `
+        <footer>
+            <div class="navigation_bar">
+                <h3>Editors' Pages</h3>
+                <a href="anjitha.html">Anjitha</a>
+                <a href="nohim.html">Nohim</a>
+                <a href="monali.html">Monali</a>
+                <a href="anjana.html">Anjana</a>
+            </div>
+        </footer>
     `)
 }); 
